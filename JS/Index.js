945207c5d03cfd84d2a9cb932d5979a774748bbd
Roberto <rmr15092004@gmail.com>
2025-03-04 11:49:02 +0100

    Primera subida

--- JS/Index.js
+++ JS/Index.js
@@ -18,19 +18,19 @@ axios.get(url)
             imageBackground
         } = response.data.data;
 
-        // Acceder a los elementos por su ID y actualizar su contenido
-        document.getElementById('businessName').textContent = nameShow; // Actualiza el nombre del negocio
-        document.getElementById('businessDescription').textContent = description; // Actualiza la descripción
-        document.getElementById('businessLogo').src = logo; // Cambia el logo
-        document.getElementById('businessAddress').textContent = address; // Actualiza la dirección
-        document.getElementById('businessEmail').textContent = email; // Actualiza el email
-        document.getElementById('businessPhone').textContent = phone; // Actualiza el teléfono
-        document.getElementById('businessPresentationText').textContent = presentationText; // Actualiza el texto de presentación
-        document.getElementById('businessTakeawayTime').textContent = takeawaytime; // Actualiza el tiempo de recogida
-        document.getElementById('businessDeliveryTime').textContent = deliveryTime; // Actualiza el tiempo de entrega
-        document.getElementById('businessHorariosText').textContent = horariosText; // Actualiza el texto de horarios
-        document.getElementById('businessVideoBackground').src = videoBackground; // Cambia el video de fondo
-        document.getElementById('businessImageBackground').src = imageBackground; // Cambia la imagen de fondo
+        
+        document.getElementById('businessName').textContent = nameShow; 
+        document.getElementById('businessDescription').textContent = description; 
+        document.getElementById('businessLogo').src = logo; 
+        document.getElementById('businessAddress').textContent = address;
+        document.getElementById('businessEmail').textContent = email; 
+        document.getElementById('businessPhone').textContent = phone; 
+        document.getElementById('businessPresentationText').textContent = presentationText;
+        document.getElementById('businessTakeawayTime').textContent = takeawaytime; 
+        document.getElementById('businessDeliveryTime').textContent = deliveryTime; 
+        document.getElementById('businessHorariosText').textContent = horariosText; 
+        document.getElementById('businessVideoBackground').src = videoBackground; 
+        document.getElementById('businessImageBackground').src = imageBackground; 
 
     })
     .catch(error => {
